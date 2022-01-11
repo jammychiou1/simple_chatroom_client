c9990c3036b7420f31762500fd7242ae73b2ef0d
jammychiou1 <jammy.chiou1@gmail.com>
2022-01-11 23:57:20 +0800

    sample client code

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net"
+    "bufio"
+    "os"
+)
+
+func main() {
+    fmt.Println(os.Args[1])
+    fmt.Println(fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]))
+    conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]))
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+    status, err := bufio.NewReader(conn).ReadString('\n')
+    fmt.Println(status)
+}
